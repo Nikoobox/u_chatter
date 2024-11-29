4a33ba91860e931350693fedab92fcf8041b701e
Nikolay Shatalov <nikolayshatalov@Nikolays-MacBook-Pro.local>
2024-11-29 17:32:12 -0500

    ui guards

--- chatter-ns-ui/src/components/auth/Login.tsx
+++ chatter-ns-ui/src/components/auth/Login.tsx
@@ -4,10 +4,16 @@ import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 
 import { Auth } from "./";
+import useLogin from "../../hooks/useLogin";
 
 const Login: FC = () => {
+  const { login, error } = useLogin();
   return (
-    <Auth submitLabel="Login" onSubmit={async () => {}}>
+    <Auth
+      submitLabel="Login"
+      onSubmit={(request) => login(request)}
+      error={error}
+    >
       <Link to="/signup" style={{ alignSelf: "center" }}>
         <MUILink>Signup</MUILink>
       </Link>
--- chatter-ns-ui/src/components/auth/Signup.tsx
+++ chatter-ns-ui/src/components/auth/Signup.tsx
@@ -1,26 +1,39 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Link as MUILink } from "@mui/material";
 
 import { Auth } from "./";
 import { useCreateUser } from "../../hooks/apollo-client";
+import { extractErrorMessage } from "../../utils/errors";
 
 const Signup: FC = () => {
   const [createUser] = useCreateUser();
+  const [error, setError] = useState<string>("");
   return (
     <Auth
       submitLabel="Signup"
       onSubmit={async ({ email, password }) => {
-        await createUser({
-          variables: {
-            createUserInput: {
-              email,
-              password,
+        try {
+          await createUser({
+            variables: {
+              createUserInput: {
+                email,
+                password,
+              },
             },
-          },
-        });
+          });
+          setError("");
+        } catch (e) {
+          const errorMessage = extractErrorMessage(e);
+          if (errorMessage) {
+            setError(errorMessage);
+            return;
+          }
+          setError("Unkwon error occured.");
+        }
       }}
+      error={error}
     >
       <Link to="/login" style={{ alignSelf: "center" }}>
         <MUILink>Login</MUILink>
